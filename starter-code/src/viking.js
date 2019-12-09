60c0628b4a7ee130273d1bacc3562b229a179026
Kennedy Aiden <20185@columbushs.com>
2019-12-09 11:15:33 -0500

    Almost Done With Vikings

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,43 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(Damage) {
+        this.health = this.health - Damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
+    recieveDamage(Damage) {
+        this.health = this.health - Damage;
+
+        if (Viking.health > 0) {
+            return name + "has revieved" + damage + "points of damage"
+        }
+        else if (Viking.health == 0) {
+            return name + "has died in act of combat"
+        }
+    }
+    attack(){
+        return this.strength
+    }
+    battleCry(){
+        return 
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
